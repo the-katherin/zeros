26a4eb6ec63acf24819bbca46897423141a373c3
the-katherin <ekaterina082612@gmail.com>
2018-02-25 17:37:45 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+ var result=0;
+
+
+while ((number/5)>=1 ) {
+    number = Math.floor(number/5);
+    result += number;
+
+}
+ return result;
 }
